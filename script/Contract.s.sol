84d7340d036efcb91badb4c6541e7ff04f88a441
tolu0x <kalejaiyetoluwalase204@gmail.com>
2023-10-23 11:32:39 +0100

    setup deploy script

--- script/Contract.s.sol
+++ script/Contract.s.sol
@@ -2,11 +2,14 @@
 pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
+import {NFT} from "src/Contract.sol";
 
-contract CounterScript is Script {
+contract ContractScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        vm.startBroadcast();
+        new NFT(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
+        vm.stopBroadcast();
     }
 }
