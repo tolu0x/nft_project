92dab0406585a6300367158b5c6e7e86dde614b1
tolu0x <kalejaiyetoluwalase204@gmail.com>
2023-10-24 19:15:07 +0100

    write test

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -1,24 +1,19 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.21;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
 
-// import {Test, console2} from "forge-std/Test.sol";
-// import {Contract} from "../src/Contract.sol";
+import {Test, console2} from "forge-std/Test.sol";
+import {Contract} from "../src/Contract.sol";
 
-// contract ContractTest is Test {
-//     Contract public contract;
+contract ContractTest is Test {
+    Contract public contract;
 
-//     function setUp() public {
-//         contract = new Contract(0x0);
-//         contract.setNumber(0);
-//     }
+    function setUp() public {
+        contract = new Contract(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
+        contract.setBaseURI('ipfs://testtest');
+    }
 
-//     function test_Increment() public {
-//         contract.increment();
-//         assertEq(contract.number(), 1);
-//     }
-
-//     function testFuzz_SetNumber(uint256 x) public {
-//         contract.setNumber(x);
-//         assertEq(contract.number(), x);
-//     }
-// }
+    function test_URI() public {
+        contract._baseURI();
+        assertFalse(contract.baseURI(), '');
+    }
+}
